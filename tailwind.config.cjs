74e4a70361680ab10074a7246f6ea6938799e7ff
Edvinas Pranka <epranka@gmail.com>
2024-11-12 14:09:41 +0200

    Upgraded tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,3 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 };
